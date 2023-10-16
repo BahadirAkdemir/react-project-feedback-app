8ba4f80c661b3dc3dce7bcf95c0eb2b173682008
BahadirAkdemir <akdemir_bahadir@hotmail.com>
2023-10-16 20:57:11 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import React, {useState} from "react";
+import Header from "./components/Header";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackForm from "./components/FeedbackForm";
+import {v4 as uuidv4} from 'uuid'
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./pages/AboutIconLink";
+import {FeedbackProvider} from  './components/context/FeedbackContext'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const loading = false
+    const showComments = true
+    const [feedback, setFeedback] = useState(FeedbackData)
+    if (loading) return <h1>Loading...</h1>
+
+    return (
+        <FeedbackProvider>
+        <Router>
+            <Header text='Hello World' bgColor={'red'}/>
+            <div className={'container'}>
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm />
+                            <FeedbackStats />
+                            <FeedbackList />
+                            <AboutIconLink />
+                        </>
+                    }>
+
+                    </Route>
+
+                    <Route path='/about' element={<AboutPage/>}/>
+
+
+                </Routes>
+            </div>
+
+
+
+        </Router>
+        </FeedbackProvider>
+    )
 }
 
-export default App;
+export default App
